1fdaf4e6dac2b86c6ed9a7839370d2eda1f909fb
Pedro Mata-Mouros <pedro.matamouros@gmail.com>
2014-02-18 11:54:37 +0000

    Updated Model to latest changes in Object. Added a Db object as delegate, for now

--- src/Model.php
+++ src/Model.php
@@ -1,134 +1,159 @@
 <?php
 /*
  *
- *	Copyright (c) 2013, Pedro Mata-Mouros <pedro.matamouros@gmail.com>
- *	All rights reserved.
+ *  Copyright (c) 2013, Pedro Mata-Mouros <pedro.matamouros@gmail.com>
+ *  All rights reserved.
  *
- *	Redistribution and use in source and binary forms, with or without
- *	modification, are permitted provided that the following conditions are met:
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
  * 
- *	 . Redistributions of source code must retain the above copyright notice,
- *		 this list of conditions and the following disclaimer.
+ *   . Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
  *
- *	 . Redistributions in binary form must reproduce the above copyright notice,
- *		 this list of conditions and the following disclaimer in the documentation
- *		 and/or other materials provided with the distribution.
+ *   . Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
  *
- *	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- *	ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- *	LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- *	CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- *	SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *	INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- *	CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- *	ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *	POSSIBILITY OF SUCH DAMAGE.
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
  *
  */
 
 namespace Sentient;
 
 /**
- * Base class with the basic object functionality.
+ * Base Model class with the basic object functionality.
  *
- * @package		 Sentient
- * @author			Pedro Mata-Mouros Fonseca <pedro.matamouros@gmail.com>
- * @copyright	 2013, Pedro Mata-Mouros Fonseca.
- * @license		 http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
- * @version		 $LastChangedRevision$
- * @link				$HeadURL$
- * Changed by	 $LastChangedBy$
- * Changed on	 $LastChangedDate$
+ * @package		Sentient
+ * @author		Pedro Mata-Mouros Fonseca <pedro.matamouros@gmail.com>
+ * @copyright	2013, Pedro Mata-Mouros Fonseca.
+ * @license		http://opensource.org/licenses/BSD-2-Clause Simplified BSD License
+ * @version		$LastChangedRevision$
+ * @link		$HeadURL$
+ * Changed by	$LastChangedBy$
+ * Changed on	$LastChangedDate$
  */
 class Model extends Object
 {
 	/**
 	 * Last known digest of the instance.
 	 */
-	private $_signature;
+	private $signature;
 
 	/** */
-	private $_autoSave;
+	private $autoSave;
 
 	/** */
-	private $_saveOnExit;
+	private $saveOnExit;
 
-	/** Indicates if the instance was changed and not yet saved (dirty == off) */
-	//private $_dirty;
 
+	/**
+	 *
+	 */
 	public function __construct()
 	{
-		$this->_signature = NULL;
-		$this->_autoSave = FALSE;
-		$this->_saveOnExit = TRUE;
-		//$this->_dirty = FALSE;
+		$this->signature = NULL;
+		$this->autoSave = FALSE;
+		$this->saveOnExit = TRUE;
+
+		static $db;
+		if ($db === NULL) {
+			$db = new Db();
+		}
+		$this->setDelegate($db);
 	}
 
-	/** */
+	/**
+	 *
+	 */
 	public function __destruct()
 	{
-		if ($this->_saveOnExit && (/*$this->_dirty || */$this->isDirty()))
+		if ($this->saveOnExit && (/*$this->_dirty || */$this->_isDirty()))
 		{
 			$this->_save();
 		}
 	}
 
-	/** */
+	/**
+	 *
+	 */
 	public function __call($name, $args)
 	{
 		if (strpos($name, 'set') === 0)
 		{
-			$var = '_' . lcfirst(substr($name, 3));
-			if ($this->$var != $args[0])
+			$key = lcfirst(substr($name, 3));
+			if ($this->$key != $args[0])
 			{
 				// Just call Object's setter to deal with it
 				parent::__call($name, $args);
-				//$this->setDirty(TRUE);
-				if ($this->_autoSave && $this->_save())
+
+				if ($this->autoSave && $this->_save()) // Save automatically
 				{
-					$this->sign(); // automatically update signature once saved
-					//$this->setDirty(FALSE);
+					$this->_sign(); // automatically update signature once saved
 				}
 			}
 		}
 	}
 
-	public function awaken()
+	public function init()
 	{
-		$this->sign();
-		//$this->setDirty(FALSE);
+
 	}
 
+	/**
+	 *
+	 */
+	public function awaken()
+	{
+		$this->_sign();
+	}
+
+	/**
+	 *
+	 */
 	public function sleep()
 	{
 		
 	}
 
-	/** */
-	public function isDirty()
+	/**
+	 *
+	 */
+	private function _isDirty()
 	{
-		return ($this->_currentSignature() != $this->_signature);
+		return ($this->_currentSignature() != $this->signature);
 	}
 
-	/** */
-	public function sign(array $moreExclusions = array())
+	/**
+	 *
+	 */
+	protected function _sign(array $moreExclusions = array())
 	{
 		// Use the setter to honour eventual observers
 		$this->setSignature($this->_currentSignature($moreExclusions));
 	}
 
-	/** */
+	/**
+	 *
+	 */
 	private function _currentSignature($moreExclusions = array())
 	{
 		$defaultExclusions = array(
-			'_autoSave',   // Model
-			'_saveOnExit', // Model
-			'_signature',  // Model
-			'_delegate',   // Object
-			'_outlets',    // Object
-			'_observers',  // Object
+			'autoSave',   // Model
+			'saveOnExit', // Model
+			'signature',  // Model
+			'delegate',   // Object
+			'outlets',    // Object
+			'observers',  // Object
 		);
 		$exclusions = array_merge($defaultExclusions, $moreExclusions);
 		$objVars = array_diff(get_object_vars($this), $exclusions);
